988f00e800ec50d032fcb92d9dfc37ea5bb5a814
random1st <voroninr@gmail.com>
2016-07-19 18:20:05 +0300

    increase test

--- tests/test_lambda.py
+++ tests/test_lambda.py
@@ -1,9 +1,11 @@
 import os
+import tempfile
 import zipfile
 
+from git import Repo
 from mock import patch
 
-from aws_clients.aws_lambda.deployment import LambdaDeployer
+from aws_clients.aws_lambda.deployment import LambdaDeployer, LambdaPackage
 from aws_clients.aws_lambda.lambda_function import LambdaFunction
 from tests.base_test import BaseTest
 from tests.mock_aws_api import AWSMock
@@ -23,16 +25,7 @@ class LambdaTest(BaseTest):
     @patch('botocore.client.BaseClient._make_api_call',
            new=AWSMock.mock_make_api_call)
     def test_lambda_deployment(self):
-        zip_file = zipfile.ZipFile('lambda.zip', "w", zipfile.ZIP_DEFLATED)
-        # zip_file.write('examples/lambda_module.py', 'lambda_module.py')
-        zip_file.close()
-        lambda_deployer = LambdaDeployer(
-            region_name=self.region_name,
-            aws_access_key_id=self.aws_access_key_id,
-            aws_secret_access_key=self.aws_secret_access_key,
-            zip_file='lambda.zip',
-            version='development',
-            aws_lambda_config={
+        aws_lambda_config={
                 'roma_api_function': {
                     'role_name': 'lambda_basic_execution',
                     'handler': 'lambda_module.lambda_handler',
@@ -41,7 +34,18 @@ class LambdaTest(BaseTest):
                     },
                     'ignored_packages': ['ipython', 'pudb']
                 }
-            }
+        }
+        git_repo = Repo.init(path=tempfile.mkdtemp())
+        zip_file = LambdaPackage(
+            aws_lambda_config, git_repo.git_dir
+        ).create_deployment_package()
+        lambda_deployer = LambdaDeployer(
+            region_name=self.region_name,
+            aws_access_key_id=self.aws_access_key_id,
+            aws_secret_access_key=self.aws_secret_access_key,
+            zip_file=zip_file,
+            version='development',
+            aws_lambda_config=aws_lambda_config
         )
 
         lambda_deployer.deploy()
