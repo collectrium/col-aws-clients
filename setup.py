06ce0bae95520dc08ef40ce68e555efce4e08637
Anton Barkovsky <anton@swarmer.me>
2016-11-16 13:34:32 +0300

    Release 0.9

--- setup.py
+++ setup.py
@@ -31,11 +31,11 @@ setup(
     packages=packages,
     install_requires=reqs,
     package_data={'': ['*.ini', '*.txt', '*.html', '*.json', '*.yml', '*.csv']},
-    version='0.8',
+    version='0.9',
     description='Collectrium AWS clients',
     url='https://github.com/collectrium/col-aws-clients',
     # use the URL to the github repo
-    download_url='https://github.com/collectrium/col-aws-clients/tarball/0.8',
+    download_url='https://github.com/collectrium/col-aws-clients/tarball/0.9',
     # I'll explain this in a second
     keywords=['AWS', 'Amazon Web Services', 'SQS', "S3", "Lambda", "APIGateway"],
     # arbitrary keywords
